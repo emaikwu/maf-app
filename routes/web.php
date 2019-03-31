fd87ccfe80485b14ca39266b73cea23363ca81fa
Emaikwu Innocent <ennosaint@outlook.com>
2019-03-31 21:47:42 +0100

    Generated migrations for all models Category, Product, Setting, Slide, About. Also, views blade files where added for both admin and clients pages controllers were also matched.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", "ClientsController@index")->name("home");
+Route::get("/products", "ClientsController@products")->name("products");
+Route::get("/products/{id}", "ClientsController@show")->name("product");
+Route::get("/contact", "ClientsController@contact")->name("contact");
+Route::get("/about", "ClientsController@about")->name("about");
+
+Route::resource("/admin/products", "ProductsController");
+Route::resource("/admin/users", "UsersController");
+Route::resource("/admin/settings", "SettingsController");
+Route::resource("/admin/categories", "CategoriesController");
+Route::resource("/admin/abouts", "AboutsController");
+Route::resource("/admin/slides", "SlidesController");
